79601cc89d6f4b2b5e51998fe0a05fbb0be34a5c
karanjotsingh-max <kjsingh@mitaoe.ac.in>
2021-07-02 20:52:51 +0530

    Redesigned Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,97 +7,100 @@ import Navigation from "./Navigation";
 const Home = () => {
     return (
         <div class="yu8 bg-white-30">
-            <Navigation Tabchange="Hospital Register" url="/hospitalregister"/>
+            <Navigation Tabchange="Hospital Register" url="/hospitalregister" />
             <Search />
-           
+
 
             <div class=" bd-callout bd-callout-info overflow-auto ">
-                <table class="bb shadow-3  f6 w-100 mw8 center" cellspacing="0">
-                    <thead class=" bg-blue ">
+                <table class="bb  shadow-3 
+                  f6 w-100 mw8 center" cellspacing="0">
+                    <thead class=" bg ">
                         <tr >
-                            <th scope="col"><big> HOSPITAL NAMEs</big>
+                            <th scope="col"><big> HOSPITAL NAMES</big>
                             </th>
-                            <th scope="col" class="tl"><big>SPECIALITY</big></th>
-                            <th scope="col" colspan="2" class="tc"><big>ACTION</big></th>
+                            <th scope="col"  ><big class="thmain">SPECIALITY</big></th>
+                            <th scope="col" colspan="2" class="tc "><big>ACTION</big></th>
                         </tr>
                     </thead>
                     <tbody >
                         {/* Ist entry in the table */}
-                        <tr class="m3 drk  hover-bg-white">
+                        <tr class="m3  drk outline " id="rd">
+                            {/* <td></td> */}
 
-                            <td ><big>XYZ Hospital </big></td>
+                            <td class="rf"><big><img class="icon" src="https://image.flaticon.com/icons/png/512/33/33777.png" alt="hosp" width="100px" />XYZ Hospital </big></td>
                             <td class="kj4"><big>__Speciality</big></td>
                             <td>
                                 <button
-                                    class="close2 btn btn-outline-primary mr-2"
+                                    class="close2 btn btn-outline-primary mr-2" id="btn1"
 
                                 >
-                                    SEND ENQUIRY
+                                    <b> SEND ENQUIRY </b>
                                 </button></td>
                             <td>
-                                <button class=" btn btn-outline-primary mr-2" >
-                                    MORE
+                                <button class=" btn btn-outline-primary mr-2" id="btn1">
+                                    <b>MORE</b>
                                 </button>
 
 
                             </td>
                         </tr>
 
- 
-                        <tr class="thead-dark drk hover-bg-white">
 
-                            <td><big>XYZ Hospital </big></td>
+                        <tr class="thead-dark  drk ">
+
+                            <td class="rf"><big><img class="icon" src="https://image.flaticon.com/icons/png/512/33/33777.png" alt="hosp" width="100px" />XYZ Hospital </big></td>
                             <td class="kj4"><big>__Speciality</big></td>
                             <td>
                                 <button
                                     class="close2 btn btn-outline-primary mr-2"
+                                    id="btn1"
 
                                 >
-                                    SEND ENQUIRY
+                                    <b>SEND ENQUIRY</b>
                                 </button></td>
                             <td>
-                                <button class="btn btn-outline-primary mr-2" >
-                                    MORE
+                                <button class="btn btn-outline-primary mr-2" id="btn1">
+                                    <b>MORE</b>
                                 </button>
 
 
                             </td>
                         </tr>
                         {/* 3rd entry */}
-                        <tr class="thead-dark drk hover-bg-white">
+                        <tr class="thead-dark outline drk ">
 
-                            <td><big>XYZ Hospital </big></td>
+                            <td class="rf"><big><img class="icon" src="https://image.flaticon.com/icons/png/512/33/33777.png" alt="hosp" width="100px" />XYZ Hospital </big></td>
                             <td class="kj4"><big>__Speciality</big></td>
                             <td>
                                 <button
-                                    class="close2 btn btn-outline-primary mr-2"
+                                    class="close2 btn btn-outline-primary mr-2" id="btn1"
 
                                 >
-                                    SEND ENQUIRY
+                                    <b>SEND ENQUIRY</b>
                                 </button></td>
                             <td>
-                                <button class="btn btn-outline-primary mr-2" >
-                                    MORE
+                                <button class="btn btn-outline-primary mr-2" id="btn1">
+                                    <b>MORE</b>
                                 </button>
 
 
                             </td>
                         </tr>
                         {/* 4th */}
-                        <tr class="thead-dark drk hover-bg-white">
+                        <tr class="thead-dark  drk ">
 
-                            <td><big>XYZ Hospital </big></td>
+                            <td class="rf"><big><img class="icon" src="https://image.flaticon.com/icons/png/512/33/33777.png" alt="hosp" width="100px" />XYZ Hospital </big></td>
                             <td class="kj4"><big>__Speciality</big></td>
                             <td class="tl">
                                 <button
-                                    class="close2 btn btn-outline-primary mr-2"
+                                    class="close2 btn btn-outline-primary mr-2" id="btn1"
 
                                 >
-                                    SEND ENQUIRY
+                                    <b>SEND ENQUIRY </b>
                                 </button></td>
                             <td>
-                                <button class="btn btn-outline-primary mr-2" >
-                                    MORE
+                                <button class="btn btn-outline-primary mr-2" id="btn1" >
+                                    <b>MORE</b>
                                 </button>
 
 
@@ -110,7 +113,7 @@ const Home = () => {
 
 
             </div>
-    
+
         </div>
 
 
