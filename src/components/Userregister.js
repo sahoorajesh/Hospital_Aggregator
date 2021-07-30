1376321a68c591f47fc0bb7ce6e816346f2898a2
sanket180501 <sanketmahajan2001@gmail.com>
2021-07-30 21:05:28 +0530

    testing done

--- src/components/Userregister.js
+++ src/components/Userregister.js
@@ -2,7 +2,30 @@ import React from "react";
 import Navigation from './Navigation';
 import { NavLink } from 'react-router-dom';
 import "./Userregister.css"
+import { useState } from "react";
 function Userregister() {
+    const [username,setName] = useState("")
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+
+    async function signUp(e)
+    {
+        e.preventDefault()
+        let item = {username,email,password,"roles":["admin"]}
+        console.warn(item)
+        
+        let result = await fetch("http://localhost:8010/api/test/admin/signup",{
+            method : "POST",
+            body :JSON.stringify(item),
+            headers : {
+                "Content-Type":"application/json; charset=utf-8",
+                "Accept":"application/json"
+            }
+        })
+        result = await result.json()
+        console.warn("result",result)
+    }
+
     return (
         <div className="body-info">
         	<Navigation Tabchange="Hospital Register" url="/hospitalregister" Tabchange1="Sign in" url1="/login"/>
@@ -13,27 +36,27 @@ function Userregister() {
                 
                 <div className="form-group">
                     <div className="input-group">
-                      
-                        <input type="text" className="form-control" name="username" placeholder="Username" required="required" />
+                        <i className="fa fa-user mr2"></i>
+                        <input type="text" value={username} onChange={(e)=>setName(e.target.value)} className="form-control" name="username" placeholder="Username" required="required" />
                     </div>
                 </div>
                 <div className="form-group">
                     <div className="input-group">
                         
-            
-                        <input type="email" className="form-control" name="email" placeholder="Email Address" required="required" />
+                        <i className="fa fa-paper-plane mr2"></i>
+                        <input type="email"  value={email} onChange={(e)=>setEmail(e.target.value)} className="form-control" name="email" placeholder="Email Address" required="required" />
                     </div>
                 </div>
                 <div className="form-group ">
                     <div className="input-group">
                        
-                       
-                        <input type="text" className="form-control" name="password" placeholder="Password" required="required" />
+                        <i className="fa fa-lock mr3"></i>
+                        <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} className="form-control" name="password" placeholder="Password" required="required" />
                     </div>
                 </div>
                 
                 <div className="form-group ">
-                    <button type="submit" className="btn btn-outline-primary">Sign Up</button>
+                    <button type="submit" onClick={signUp}  className="btn btn-outline-primary">Sign Up</button>
                 </div>
 
                 <div className="text-center black">Already have an account? <NavLink to="/login">Login here</NavLink></div>
